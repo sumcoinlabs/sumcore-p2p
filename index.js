c9dadb3aeb3200f2f1ec4f90bc3769ec6c5bf380
Charlie Lee <charlie@coinbase.com>
2016-09-02 15:50:00 -0700

    Litecoin: modify for Litecoin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var bitcore = require('bitcore-lib');
-bitcore.P2P = require('./lib');
+var litecore = require('litecore-lib');
+litecore.P2P = require('./lib');
 
-module.exports = bitcore.P2P;
+module.exports = litecore.P2P;
--- test/messages/message.js
+++ test/messages/message.js
@@ -3,7 +3,7 @@
 var should = require('chai').should();
 var P2P = require('../../');
 var Message = P2P.Messages.Message;
-var Networks = require('bitcore-lib').Networks;
+var Networks = require('litecore-lib').Networks;
 
 describe('Message', function() {
 
@@ -31,7 +31,7 @@ describe('Message', function() {
         return new Buffer(0);
       };
       var buffer = message.toBuffer();
-      var expectedBuffer = new Buffer('f9beb4d9636f6d6d616e640000000000000000005df6e0e2', 'hex');
+      var expectedBuffer = new Buffer('fbc0b6db636f6d6d616e640000000000000000005df6e0e2', 'hex');
       buffer.should.deep.equal(expectedBuffer);
     });
   });
